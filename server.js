072e6d10d71df746bb57cfbe7f320a8bdeaa8637
Amado Gutierres <amadocardenas0@gmail.com>
2021-07-13 20:34:08 -0600

    created user

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
-const path = require('path');
+const path = require('path')
 const exphbs = require('express-handlebars');
 const session = require('express-session')
+const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
+const routes = require('./controllers/');
+const sequelize = require('./config/connection');
 
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: 'Super secret secret',
+    secret: 'Secret secret secret',
     cookie: {},
     resave: false,
     saveUninitialized: true,
@@ -30,4 +33,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => console.log('Now listening.')); 
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening.'));
+});
