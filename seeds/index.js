95aa70ec7e835d57a5329b0abe3cc6a407f5404f
Amado Gutierres <amadocardenas0@gmail.com>
2021-07-21 21:54:58 -0600

    umm actually fixed seeds

--- seeds/index.js
+++ seeds/index.js
@@ -7,13 +7,14 @@ const sequelize = require('../config/connection');
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('--------------');
-  await seedUsers();
+
+  seedUsers();
   console.log('--------------');
 
-  await seedPosts();
+ seedPosts();
   console.log('--------------');
 
-  await seedComments();
+ seedComments();
   console.log('--------------');
 
   process.exit(0);
